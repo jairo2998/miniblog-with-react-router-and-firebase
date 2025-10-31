317875c79d75a4b7e072dda35f6156fd9f6a2796
Jairo Mendes <jairo2998@gmail.com>
2025-10-31 12:59:28 -0300

    feat: Implement logic for new post creation in Firestore

--- src/pages/CreatePost/CreatePost.jsx
+++ src/pages/CreatePost/CreatePost.jsx
@@ -1,9 +1,114 @@
 import styles from './CreatePost.module.css';
 
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { useAuthValue } from '../../context/AuthContext';
+import { useInsertDocument } from '../../hooks/useInsertDocument';
+
 const CreatePost = () => {
+
+  const [title, setTitle] = useState("");
+  const [image, setImage] = useState("");
+  const [body, setBody] = useState("");
+  const [tags, setTags] = useState([]);
+  const [formError, setFormError] = useState("");
+
+  const { user } = useAuthValue();
+  
+
+  const {insertDocument, response} = useInsertDocument("posts");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("yeah, its here");
+    
+    setFormError("");
+    // validate image URL
+    {/*
+    try {
+      new URL(image);
+    } catch (error) {
+      setFormError("A URL da imagem é inválida.");
+      return;
+    }
+    // create tags array
+    const tagsArray = tags.split(",").map((tag) => tag.trim().toLowerCase());
+    // check all values
+    if (!title || !image || !body || !tags) {
+      setFormError("Por favor, preencha todos os campos.");
+      return;
+    }
+
+    */}
+    insertDocument({
+      title,
+      image,
+      body,
+      tags, 
+      uid: user.uid,
+      createdBy: user.displayName
+    });
+    
+    // redirect to home page
+    //navigate("/");
+
+  }
+
   return (
     <div className={styles.createPost}>
-      <h1>Criar Post</h1>
+      <h2>Criar Post</h2>
+      <p>Crie um novo post preenchendo os campos abaixo:</p>
+      <form onSubmit={handleSubmit}>
+        <label>
+          <span>Título:</span>
+          <input
+            type="text"
+            name="title"
+            required
+            placeholder="Título do post"
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+          />
+        </label>
+
+        <label>
+          <span>Imagem:</span>
+          <input
+            type="text"
+            name="image"
+            required
+            placeholder="URL da imagem"
+            value={image}
+            onChange={(e) => setImage(e.target.value)}
+          />
+        </label>
+
+        <label>
+          <span>Conteúdo:</span>
+          <textarea
+            name="body"
+            required
+            placeholder="Conteúdo do post"
+            value={body}
+            onChange={(e) => setBody(e.target.value)}
+          />
+        </label>
+
+        <label>
+          <span>Tags:</span>
+          <input
+            type="text"
+            name="tags"
+            placeholder="Tags do post (separadas por vírgula)"
+            value={tags}
+            onChange={(e) => setTags(e.target.value.split(","))}
+          />
+        </label>
+
+        {!response.loading && <button className="btn">Criar Post</button>}
+        {response.loading && <button className="btn" disabled>Aguarde...</button>}
+        {response.error && <p className="error">{response.error}</p>}
+      </form>
     </div>
   )
 }
